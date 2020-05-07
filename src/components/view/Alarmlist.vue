77c720d2e64624a4dfffd6051523d93f06c05c7b
grimmi22 <40334510+grimmi22@users.noreply.github.com>
2020-05-07 20:47:52 +0200

    Alarmlist notifications.js change Date Format from ISO to LocalString

--- src/components/view/Alarmlist.vue
+++ src/components/view/Alarmlist.vue
@@ -1,41 +1,47 @@
 <template>
-
-  <v-data-table
+  <div>
+  <v-data-table light
     :headers="headers"
     :items="getEventList"
     :items-per-page="20"
     class="elevation-1"
   ></v-data-table>
-
+  </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+  import {mapActions, mapGetters} from "vuex";
+
 
 
     export default {
       data() {
         return {
-        headers: [
-          {
-            text: 'Alarm',
-            align: 'start',
-            sortable: false,
-            value: 'description',
-          },
-          { text: 'DatumZeit', value: 'timeStamp' },
-          { text: 'Objekt Name', value: 'objectName' },
-          { text: 'Aktueller Wert', value: 'presentValue' },
-          { text: 'Zustand', value: 'toState' },
-        ]
-}
-},
+          headers: [
+            {
+              text: 'Alarm',
+              align: 'start',
+              sortable: false,
+              value: 'description',
+            },
+            {text: 'DatumZeit', value: 'timeStamp'},
+            {text: 'Objekt Name', value: 'objectName'},
+            {text: 'Aktueller Wert', value: 'presentValue'},
+            {text: 'Zustand', value: 'toState'},
+          ]
+        }
+      },
 
-computed: {
-  ...mapGetters(["getEventList"])
-  }
-}
+      computed: {
+        ...mapGetters(["getEventList"])
+      },
+      methods: {
+        ...mapActions([
 
+
+        ]),
+      }
+    };
 </script>
 
 <style scoped>
