2a7264115473b15c8500fd236bc29a1c6a073946
andreasvogt89 <30302212+andreasvogt89@users.noreply.github.com>
2020-05-16 16:06:33 +0200

    -changes in devices handling

--- src/store/modules/devices.js
+++ src/store/modules/devices.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 export const state = {
   devices: [],
-  syncdDevices: [],
   devicesInitialized: false
 };
 
@@ -17,18 +16,32 @@ export const actions = {
     commit('setDevices', response.data)
   },
 
+  async preloadDevices({
+                      commit
+                    }) {
+    const response = await axios.get(
+        "http://localhost:8098/preload/devices"
+    );
+    commit('preloadDevices', response.data)
+  },
 
-  async sendDevices( {commit}, selectedDevices) {
+
+  async sendDevices({commit}, selectedDevices) {
     commit('syncDevices', selectedDevices);
     axios.post(
       "http://localhost:8098/devices", selectedDevices
-    )
+    ).then(res => {
+      if(res.status === 200) {
+        this.preloadDevices();
+      }
+    });
   }
 };
 
 export const mutations = {
+  syncDevices: (state, devices) => (state.syncdDevices = devices),
   setDevices: (state, devices) => (state.devices = devices),
-  syncDevices: (state, devices) => (state.syncdDevices = devices)
+  preloadDevices: (state, devices) => (state.devices = devices)
 };
 
 export const getters = {
